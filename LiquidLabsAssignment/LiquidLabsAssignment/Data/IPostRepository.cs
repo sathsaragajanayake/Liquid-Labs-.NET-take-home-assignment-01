790472bddc4a949b66168bf5e805d751a34a88ec
Sathsara Gajanayake <gpssara97@gmail.com>
2026-09-18 13:05:02 +0530

    fixing a warning

--- LiquidLabsAssignment/LiquidLabsAssignment/Data/IPostRepository.cs
+++ LiquidLabsAssignment/LiquidLabsAssignment/Data/IPostRepository.cs
@@ -5,7 +5,7 @@ namespace LiquidLabsAssignment.Data
     public interface IPostRepository
     {
         Task<List<Post>> GetPostsAsync();
-        Task<Post> GetPostByIdAsync(int id);
+        Task<Post?> GetPostByIdAsync(int id);
         Task AddPostAsync(Post post);
     }
 }
